16a13522e30b9be11abcba9c26504aff8998e376
Michael Chang <chang.michael1009@gmail.com>
2019-09-15 18:27:57 -0400

    Fixed the yaml file so that DB and docker-compose model are on the backend network. Also ran initial migrations for jersey and added jersey app to settings.py

--- isa/jersey/models.py
+++ isa/jersey/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    SHIRT_SIZES = (
+        ('XS', "Extra Small"),
+        ('S', "Small"),
+        ('M', "Medium"),
+        ('L', "Large"),
+        ('XL', "Extra Large"),
+        ('XXL', "Double Extra Large"),
+    )
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+    def __str__(self):
+        return self.email
+
+
+class Jersey(models.Model):
+    team = models.CharField(max_length=60)
+    number = models.PositiveIntegerField()
+    player = models.CharField(max_length=60)
+    SHIRT_SIZES = (
+        ('XS', "Extra Small"),
+        ('S', "Small"),
+        ('M', "Medium"),
+        ('L', "Large"),
+        ('XL', "Extra Large"),
+        ('XXL', "Double Extra Large"),
+    )
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    primary_color = models.CharField(max_length=60)
+    secondary_color = models.CharField(max_length=60)
