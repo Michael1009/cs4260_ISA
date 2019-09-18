8f6e0a265c121c1ac01d9123ad57f70c95bd54c5
Karim Shoorbajee <karim.shoorbajee@thinkingwithtools.com>
2019-09-18 17:34:30 -0400

    Added post for jersey

--- isa/jersey/views.py
+++ isa/jersey/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import User, Jersey
+import json 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the jersey index.")
+
+def create_jersey(request):
+    f= open("wsgi_debug.txt","w+")
+    f.write(json.dumps(dict(request.POST)))
+    f.close()
+    new_jersey = Jersey(
+        team = request.POST['team'],
+        number = request.POST['number'],
+        player = request.POST['player'],
+        shirt_size = request.POST['shirt_size'],
+        primary_color = request.POST['primary_color'],
+        secondary_color = request.POST['secondary_color']
+        ) 
+    new_jersey.save()
