7cfb0c6794a50381affc003a0285a6184048b7ef
Michael Chang <chang.michael1009@gmail.com>
2019-09-21 12:09:46 -0400

    Added error checking to create_jersey, wrote test for create_user, fixed URLs to have v1 in them

--- isa/jersey/tests.py
+++ isa/jersey/tests.py
@@ -11,10 +11,21 @@ class JerseyTest(TestCase):
         self.client = Client()
         logged_in = self.client.login(username='www', password='$3cureUS')
 
-    def test_createJersey(self):
-        # Issue a GET request.
+    def test_createUser(self):
         response = self.client.post(
-            '/jersey/api/Jersey/create',
+            '/jersey/api/v1/User/create',
+            {
+                'email': 'l.da@gmail.com',
+                'first_name': 'Leonardo',
+                'last_name': 'DaVinci',
+                'shirt_size': 'XXL',
+            })
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_createJersey(self):
+        response = self.client.post(
+            '/jersey/api/v1/Jersey/create',
             {
                 'team': 'ExampleTeam',
                 'number': '1',
--- isa/jersey/views.py
+++ isa/jersey/views.py
@@ -11,6 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 def index(request):
     return HttpResponse("Hello, world. You're at the jersey index.")
 
+
 @csrf_exempt
 def create_user(request):
     if request.method == "POST":
@@ -23,23 +24,31 @@ def create_user(request):
                 shirt_size=request.POST['shirt_size']
             )
             new_user.save()
-        except: 
+        except:
             result = json.dumps(
                 {'error': 'Missing field or malformed data in CREATE request. Here is the data we received: {}'.format(form), 'ok': False})
             return HttpResponse(result)
 
     return HttpResponse(status=201)
 
+
 @csrf_exempt
 def create_jersey(request):
-    new_jersey = Jersey(
-        team=request.POST['team'],
-        number=request.POST['number'],
-        player=request.POST['player'],
-        shirt_size=request.POST['shirt_size'],
-        primary_color=request.POST['primary_color'],
-        secondary_color=request.POST['secondary_color']
-    )
-    new_jersey.save()
+    if request.method == "POST":
+        form = request.POST
+        try:
+            new_jersey = Jersey(
+                team=request.POST['team'],
+                number=request.POST['number'],
+                player=request.POST['player'],
+                shirt_size=request.POST['shirt_size'],
+                primary_color=request.POST['primary_color'],
+                secondary_color=request.POST['secondary_color']
+            )
+            new_jersey.save()
+        except:
+            result = json.dumps(
+                {'error': 'Missing field or malformed data in CREATE request. Here is the data we received: {}'.format(form), 'ok': False})
+            return HttpResponse(result)
 
     return HttpResponse(status=201)
