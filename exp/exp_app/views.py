afad97f094b94a8d26229482857a1d3f52efb465
Karim Shoorbajee <karim.shoorbajee@thinkingwithtools.com>
2019-10-07 00:11:10 -0400

    really basic probably kinda bad 404 handling

--- exp/exp_app/views.py
+++ exp/exp_app/views.py
@@ -22,9 +22,12 @@ def home(request):
 @csrf_exempt
 def jersey_detail(request, id):
     data = None
-    code = None
-    with urllib.request.urlopen('http://models:8000/jersey/api/v1/Jersey/'+str(id)) as response:
-        data = json.dumps(response.read().decode('UTF-8'))
-        code = response.getcode()
+    try:
+        with urllib.request.urlopen('http://models:8000/jersey/api/v1/Jersey/'+str(id)) as response:
+            data = json.dumps(response.read().decode('UTF-8'))
+    except:
+        result = json.dumps(
+            {'error': '404', 'ok': False})
+        return HttpResponse(result, content_type='application/json', status=404)
     result = json.loads(data)
     return HttpResponse(result)
--- web/web_app/views.py
+++ web/web_app/views.py
@@ -9,16 +9,11 @@ def index(request):
 
 def item_detail(request,id):
     data = None
-    code = None
     template = None
     with urllib.request.urlopen('http://exp:8000/exp/jersey_detail/'+str(id)) as response:
         data = response.read().decode('UTF-8')
-        code = response.getcode()
-    if code == 404:
-        template = 'web_app/404.html'
-    elif code == 200:
-        context = {
-            'jersey' : json.loads(data)[0]['fields']
-        }
-        template = 'web_app/item_detail.html'
+    context = {
+        'jersey' : json.loads(data)[0]['fields']
+    }
+    template = 'web_app/item_detail.html'
     return render(request,template,context)
