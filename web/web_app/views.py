de7a53136174b365fd9242b5aba909c3d5025b7b
davio606 <djy6cg@virginia.edu>
2019-10-07 03:14:48 -0400

    Made home page with all Players listed and a picture

--- web/web_app/views.py
+++ web/web_app/views.py
@@ -5,7 +5,19 @@ import urllib.request, json
 # Create your views here.
 
 def index(request):
-    return render(request,'web_app/index.html')
+    data = None
+    with urllib.request.urlopen('http://exp:8000/exp/home/') as response:
+        data = response.read().decode('UTF-8')
+    myList = []
+    i = 0
+    for fields in json.loads(data):
+        bigData = json.loads(data)[i]['fields']
+        i = i + 1
+        myList.append(bigData)
+    context = {
+        'jerseys' : myList
+    }
+    return render(request,'web_app/index.html',context)
 
 def item_detail(request,id):
     data = None
