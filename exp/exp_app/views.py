ca4cc30e37ba74edf456e35fdc6aed986669336d
Karim Shoorbajee <karim.shoorbajee@thinkingwithtools.com>
2019-11-21 18:39:54 -0500

    fixed the search i think

--- exp/exp_app/views.py
+++ exp/exp_app/views.py
@@ -210,11 +210,18 @@ def create_item(request):
 def search(request):
     query = request.GET.get('query')
     es = Elasticsearch(['es'])
-    response = es.search(index='jersey_index', body={'query': {'simple_query_string': {'query': query}}, 'size': 10})
-    return_result = json.dumps({
-        'ok' : True, 
-        'result': response['hits']['hits']
-    })
+    try:
+        response = es.search(index='jersey_index', body={"query": {"function_score": {"query": {"query_string": {"query": query}}}}})
+        return_result = json.dumps({
+            'ok' : True, 
+            'result': response['hits']['hits']
+        })
+    except:
+         return_result = json.dumps({
+            'ok' : True, 
+            'result': []
+        })
+    
     return HttpResponse(return_result, content_type='application/json')
         
 
