bf48a68405977d1f372892530aa69a19c263e44a
Karim Shoorbajee <karim.shoorbajee@thinkingwithtools.com>
2019-11-21 18:00:09 -0500

    get old version

--- selenium/selenium_tests.py
+++ selenium/selenium_tests.py
@@ -7,17 +7,13 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 class JerseyMarketTest(unittest.TestCase):
 
     def setUp(self):
-        # chrome_options = Options()
-        # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = None
         while self.driver is None:
+            
             try:
                 self.driver = webdriver.Remote(
                     command_executor='http://selenium-chrome:4444/wd/hub',
                     desired_capabilities=DesiredCapabilities.CHROME,
-                    # chrome_options=chrome_options
                 )
             except:
                 pass
