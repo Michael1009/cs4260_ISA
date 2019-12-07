15fe18dd1563a68477b35a6f05b35f3bf996e5b1
davio606 <djy6cg@virginia.edu>
2019-12-06 23:51:46 -0500

    settings.py and yaml

--- isa/jersey/views.py
+++ isa/jersey/views.py
@@ -16,39 +16,6 @@ import datetime
 def index(request):
     return HttpResponse("Hello, world. You're at the jersey index.")
 
-<<<<<<< Updated upstream
-=======
-
-def incorrect_REST_method(method):
-    return json.dumps(
-        {'error': 'Incorrect REST method used. This endpoint expects a {} request'.format(
-            method), 'ok': False}
-    )
-
-
-@csrf_exempt
-def create_user(request):
-    if request.method == "POST":
-        form = request.POST
-        try:
-            new_user = User(
-                email=request.POST['email'],
-                first_name=request.POST['first_name'],
-                last_name=request.POST['last_name'],
-                shirt_size=request.POST['shirt_size']
-            )
-            new_user.save()
-            return HttpResponse(status=201)
-        except:
-            result = json.dumps(
-                {'error': 'Missing field or malformed data in CREATE request. Here is the data we received: {}'.format(form), 'ok': False})
-            return HttpResponse(result)
-    else:
-        result = json.dumps(
-            {'error': 'Incorrect REST method used. This endpoint expects a POST request', 'ok': False}
-        )
-        return HttpResponse(result)
->>>>>>> Stashed changes
 
 def incorrect_REST_method(method):
     result = json.dumps(
