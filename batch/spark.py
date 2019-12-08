e1a0dc96079cb6a09623d23c5953328bfd01e4e9
Michael Chang <chang.michael1009@gmail.com>
2019-12-08 12:20:10 -0500

    Small fixes

--- batch/spark.py
+++ batch/spark.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext
 
 
-def allPairs(item):
+def getPairs(item):
     item_list = list(item)
     return [(item_list[i], item_list[j]) for i in range(len(item_list)) for j in range(i+1, len(item_list))]
 
@@ -12,18 +12,17 @@ sc = SparkContext("spark://spark-master:7077", "PopularItems")
 data = sc.textFile("view_count.log", 2)
 # Filter out all duplicate rows from RDD
 data = data.distinct()
-
 # Tell each worker to split each line of its partition
 pairs = data.map(lambda line: line.split(","))
 
 # Group data into (user_id, [item1, item2, item3, etc.])
 user_group_items = pairs.groupByKey()
-# Just get the list of items ex. [item1, item2, etc.]
+# Get the list of items ex. [item1, item2, etc.]
 items = user_group_items.map(lambda x: x[1])
-# This should get all of the pairs ex. [(item1, item2), (item1, item3), etc.]
-coclicks = items.flatMap(lambda item: allPairs(item))
+# Get all of the pairs ex. [(item1, item2), (item1, item3), etc.]
+coclicks = items.flatMap(lambda item: getPairs(item))
 # Map each coclick to a value of 1 ex. ((item1, item2), 1)
-coclicks = coclicks.map(lambda x: (x, 1))
+coclicks = coclicks.map(lambda item: (item, 1))
 # Use reduceByKey to get the counts of them ex. ((item1, item2), 4)
 count = coclicks.reduceByKey(lambda x, y: int(x)+int(y))
 # Filter to remove the ones with less than 3 co-clicks
