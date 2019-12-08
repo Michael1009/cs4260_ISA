adcbd1603715235d7e9d8a21c17991fefb64b619
Michael Chang <chang.michael1009@gmail.com>
2019-12-08 00:25:52 -0500

    More work on spark script

--- batch/spark.py
+++ batch/spark.py
@@ -1,33 +1,33 @@
 from pyspark import SparkContext
 
+
+def allPairs(item):
+    item_list = list(item)
+    return [(item_list[i], item_list[j]) for i in range(len(item_list)) for j in range(i+1, len(item_list))]
+
+
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
-# 1. Read data in as pairs of (user_id, item_id clicked on by the user)
-# each worker loads a piece of the data file
+# Each worker loads a piece of the data file
 data = sc.textFile("view_count.log", 2)
-# tell each worker to split each line of it's partition
+# Filter out all duplicate rows from RDD
+data = data.distinct()
+
+# Tell each worker to split each line of its partition
 pairs = data.map(lambda line: line.split(","))
 
-# 2. Group data into (user_id, list of item ids they clicked on)
-group_by_key = pairs.groupByKey()
-
-# 3. Transform into (user_id, (item1, item2)
-coview = group_by_key.flatMap(lambda x: map(lambda e: (x[0], e), x[1]))
-
-# 4. Transform into ((item1, item2), list of user1, user2 etc)
-# where users are all the ones who co-clicked (item1, item2)
-
-# 5. Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2)
-
-
-# 6. Filter out any results where less than 3 users co-clicked the same pair of items
-
-# re-layout the data to ignore the user id
-pages = pairs.map(lambda pair: (pair[1], 1))
-# shuffle the data so that each key is only on one worker
-
-# and then reduce all the values by adding them together
-count = pages.reduceByKey(lambda x, y: int(x)+int(y))
+# Group data into (user_id, [item1, item2, item3, etc.])
+user_group_items = pairs.groupByKey()
+# Just get the list of items ex. [item1, item2, etc.]
+items = user_group_items.map(lambda x: x[1])
+# This should get all of the pairs ex. [(item1, item2), (item1, item3), etc.]
+coclicks = items.flatMap(lambda item: allPairs(item))
+# Map each coclick to a value of 1 ex. ((item1, item2), 1)
+coclicks = coclicks.map(lambda x: (x, 1))
+# Use reduceByKey to get the counts of them ex. ((item1, item2), 4)
+count = coclicks.reduceByKey(lambda x, y: int(x)+int(y))
+# Filter to remove the ones with less than 3 co-clicks
+count = count.filter(lambda x: x[1] >= 3)
 
 # bring the data back to the master node so we can print it out
 output = count.collect()
